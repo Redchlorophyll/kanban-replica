9ca6823657ebc851cb1c827705df6a329327f30d
redchlorophyll <hendra.dhonni.888@gmail.com>
2023-01-18 22:17:46 +0700

    feat: test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function navbar() {
   return (
     <div className="top-0 w-full h-[60px] bg-green-400 flex justify-center">
-      <div className="translate-y-[15px] text-xl">navbar</div>
+      <div className="translate-y-[15px] text-xl bg-red-100">navbar</div>
     </div>
   );
 }
