a223ca7815daea11b1ceaf170eaf75c003d4ac04
redchlorophyll <hendra.dhonni.888@gmail.com>
2023-01-19 17:21:08 +0700

    feat: create button ui

---
+++ components/Button.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode, useEffect, useState } from "react";
+
+type buttonProps = {
+  variant?: "primary" | "danger" | "outline";
+  children?: ReactNode;
+  onClick?: () => void;
+};
+
+type buttonStyle = {
+  variant?: "primary" | "danger" | "outline";
+};
+
+export default function Button({
+  variant = "outline",
+  children = "Button",
+  onClick,
+}: buttonProps) {
+  const [style, setStyle] = useState<string>("");
+
+  const btnVariant = {
+    primary: "bg-[#01959F] font-bold text-white",
+    danger: "bg-[#E11428] font-bold text-white",
+    outline:
+      "outline outline-1 outline-[#E0E0E0] bg-white font-bold text-black",
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      className={`p-[4px_16px_4px_16px] rounded-lg shadow-[0_1px_2px_rgba(0,0,0,0.12)] ${btnVariant[variant]}`}
+    >
+      {children}
+    </button>
+  );
+}
